54fe2c3084e3b1b7bf16bfc44c50f79251972a84
safrurozaky <safrurozaky17@gmail.com>
2022-10-23 15:56:33 +0700

    finishgit status

--- lib/main.dart
+++ lib/main.dart
@@ -52,32 +52,163 @@ class _MyWidgetState extends State<MyWidget> {
               ),
             ),
           ),
-          //membuat tulisan dan dikasih jarak vertikal dan horizontalnya
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 35.0, horizontal: 25.0),
-            child: Text(
-              'Transalte these phrases to english',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          //membuat box kotak yang di tumpuk ke stack, dengan disesuaikan jarak marginnya
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 150, horizontal: 20),
-            height: 240,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  spreadRadius: .2,
-                  blurRadius: 50,
-                  offset: Offset(0, 20), // untuk rubah posisi shadow
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              children: [
+                //membuat tulisan dan dikasih jarak vertikal dan horizontalnya
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 35.0, horizontal: 25.0),
+                  child: Text(
+                    'Transalte these phrases to english',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                //membuat box kotak yang di tumpuk ke stack, dengan disesuaikan jarak marginnya
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  height: 240,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: .2,
+                        blurRadius: 50,
+                        offset: Offset(0, 5), // untuk rubah posisi shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(25.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.format_quote_outlined,
+                              size: 30,
+                            ),
+                            Icon(
+                              Icons.message,
+                              size: 30,
+                              color: Colors.blue,
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          'Buenas noches,',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          '¿cómo estuvo su día? ',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.format_quote_outlined,
+                              size: 30,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.blue,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.grey,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.grey,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.grey,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 0.1,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "Your answer...",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 25, top: 25),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.check_circle_sharp,
+                        size: 50,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            color: Colors.blue,
+                            blurRadius: 50.0,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
